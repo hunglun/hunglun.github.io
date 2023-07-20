f24f0253b7175de7311cbe4fbb029754b37567d9
O Hung Lun <hunglun.o@gmail.com>
2023-07-20 05:01:17 +0000

    added  a list of projects deploy to Github Page

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,10 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import projects from './components/projects';
 
-const projects = [
-  { title: "Notes Keeper", url:"keeper", description: "Note taking app with blockchain backend [WIP]" },
-  { title: "Tic Tae Toe", url:"tic-tac-toe-with-reactjs", description: "Tic Tae Toe game with AI player [WIP]" }
-];
 function App() {
   return (
     <div className="App">
@@ -13,8 +10,8 @@ function App() {
         <h1>Personal Projects 
         </h1>
         
-        {projects.map((p) => { <a href={p.url} className="App-link">{p.title}</a> })}
-        <a href={projects[0].url} className="App-link">{projects[0].title}</a>
+        {projects.map(p =>   <a href={p.url} className="App-link" alt={p.description}>{p.title}</a>)}
+      
         
       </header>
       <body>
---
+++ personal-site/src/components/projects.js
@@ -0,0 +1,7 @@
+
+const projects = [
+    { title: "Notes keeper on blockchain", url:"keeper", description: "Note taking app with blockchain backend [WIP]" },
+    { title: "AI-enabled Tic Tae Toe", url:"tic-tac-toe-with-reactjs", description: "Tic Tae Toe game with AI player [WIP]" }
+  ];
+
+export default projects;
