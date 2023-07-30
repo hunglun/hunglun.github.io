abf0e8890eac9ebdb53cb18d4b05ae690b75ec86
O Hung Lun <hunglun.o@gmail.com>
2023-07-30 16:43:53 +0000

    added freedom countdown

--- personal-site/src/components/projects.js
+++ personal-site/src/components/projects.js
@@ -1,7 +1,9 @@
 
 const projects = [
     { title: "Notes keeper on blockchain", url:"keeper", description: "Note taking app with blockchain backend [WIP]" },
-    { title: "AI-enabled Tic Tae Toe", url:"tic-tac-toe-with-reactjs", description: "Tic Tae Toe game with AI player [WIP]" }
+    { title: "AI-enabled Tic Tae Toe", url:"tic-tac-toe-with-reactjs", description: "Tic Tae Toe game with AI player [WIP]" },
+    { title: "Freedom Countdown", url:"freedom-countdown", description: "Countdown from start date to target date with Progress Bar" }
+ 
   ];
 
 export default projects;
